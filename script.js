1b16749a0fab9a8baf50be2c9c524f69ee8387de
rural855 <pedro.balduino.neto@escola.pr.gov.br>
2024-10-16 16:19:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//Elementos do DOM
+const musicForm = document.getElementByld('musicForm');
+const musicTableBody = document.querySelector('#music Table tbody');
+const exportBtn = document.getElementtByld('exporBtn'); 
+
+//Função para adicionar uma linha na tabela
+musicForm.addEventlistener('dubmit, function(event){}
+event.preventDeffault();
